594c4940a4453c6af8bc3cd7f39b883646462055
priyankastro <priyankastro7@gmail.com>
2023-07-12 03:05:15 +0530

    import module added

---
+++ imports.tf
@@ -0,0 +1,8 @@
+import {
+  to = azurerm_resource_group.rg1
+  id = "/subscriptions/1988e0e4-7595-4e9a-91cb-242d277198f0/resourceGroups/rg-priyankadget"
+}
+import {
+  to = azurerm_resource_group.rg2
+  id = "/subscriptions/1988e0e4-7595-4e9a-91cb-242d277198f0/resourceGroups/rg-TR"
+}
