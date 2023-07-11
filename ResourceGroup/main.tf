17d50faf9bc16f08965c739dc3f57d249b96621a
priyankastro <priyankastro7@gmail.com>
2023-07-12 01:39:36 +0530

    clear repo

--- ResourceGroup/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-${lower(var.resource_group_name)}-${var.resource_group_location}"
-  location = var.resource_group_location
-}
--- variables.tf
+++
@@ -1,8 +0,0 @@
-# variable "resource_group_name" {
-#   type = string
-#   description = "Enter base name (Resource Group)"
-# }
-# variable "resource_group_location" {
-#   type = string
-#   description = "Enter Resource Group location"
-# }
