48a10106341f801b4f559ad6f9ce4447c191b9ad
priyankastro <priyankastro7@gmail.com>
2023-07-11 22:39:49 +0530

    first commit

---
+++ AppServicePlan/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
